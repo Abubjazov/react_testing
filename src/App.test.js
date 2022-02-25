2b751d48a19ffda6be4e2a7af0dc2cc9bb07faca
abubjazov <abubjazov@mail.ru>
2022-02-25 20:56:21 +0300

    proj: select elements

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,14 @@ import { App } from './App'
 
 test('render App - root component', () => {
   render(<App />)
+
   const gwElem = screen.getByText(/goodby world/i)
+  const btn = screen.getByRole('button')
+  const input = screen.getByPlaceholderText(/input value/i)
 
   expect(gwElem).toBeInTheDocument()
+  expect(btn).toBeInTheDocument()
+  expect(input).toBeInTheDocument()
+
+  screen.debug()
 })
