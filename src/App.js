b70b899ca9af30215593a9716a8167a99e561d3c
abubjazov <abubjazov@mail.ru>
2022-02-25 21:20:57 +0300

    proj: async testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 export const App = () => {
+  const [data ,setData] = useState(null)
+
+  useEffect(() => {
+    const stOne = setTimeout(() => {
+      setData({})
+    }, 1000)
+
+    return () => {
+      clearTimeout(stOne)
+    }
+  }, [])
+
   return (
     <div className='App'>
+      {data && <div>DATA</div>}
       <h1>Goodby world</h1>
       <button>Kick me!!!</button>
       <input type={'text'} placeholder={'input value...'}></input>
--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,28 @@
 import { render, screen } from '@testing-library/react'
 import { App } from './App'
 
-test('render App - root component', () => {
-  render(<App />)
+describe('TEST APP', () => {
+  test('render App - root component', () => {
+    render(<App />)
 
-  const gwElem = screen.getByText(/goodby world/i)
-  const btn = screen.getByRole('button')
-  const input = screen.getByPlaceholderText(/input value/i)
+    const gwElem = screen.getByText(/goodby world/i)
+    const btn = screen.getByRole('button')
+    const input = screen.getByPlaceholderText(/input value/i)
 
-  expect(gwElem).toBeInTheDocument()
-  expect(btn).toBeInTheDocument()
-  expect(input).toBeInTheDocument()
+    expect(gwElem).toBeInTheDocument()
+    expect(btn).toBeInTheDocument()
+    expect(input).toBeInTheDocument()
 
-  expect(input).toMatchSnapshot()
+    expect(input).toMatchSnapshot()
+  })
 
-  screen.debug()
+  test('render App - screen', async () => {
+    render(<App />)
+
+    const gwElem1 = screen.queryByText(/goodby2/i)
+    expect(gwElem1).toBeNull()
+
+    const gwElem2 = await screen.findByText(/data/i)
+    expect(gwElem2).toBeInTheDocument()
+  })
 })
