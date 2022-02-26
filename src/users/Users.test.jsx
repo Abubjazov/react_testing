cfb52b6308420728bd1fb5a874ace350de2cf35d
abubjazov <abubjazov@mail.ru>
2022-02-26 19:37:44 +0300

    Users/: +Users.test

--- src/users/Users.test.jsx
+++ src/users/Users.test.jsx
@@ -1,5 +1,4 @@
-import { fireEvent, getDefaultNormalizer, render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
 import axios from 'axios'
 
 import { Users } from './Users'
