55cb2f416f78fe86cc7f363c9c888ac88f3d8258
abubjazov <abubjazov@mail.ru>
2022-02-26 17:59:04 +0300

    proj: +Users

---
+++ src/users/Users.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const Users = () => {
+	const [users, setUsers] = useState([])
+
+	const loadUsers = async () => {
+		const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+		setUsers(response.data)
+	}
+
+	useEffect(() => {
+		loadUsers()
+	}, [])
+
+	return (
+		<div>
+			{users.map(user => <div key={user.id}>{user.name}</div>)}
+		</div>
+	)
+}
+
+export default Users
