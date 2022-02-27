0f157484f694311b38442cd045d4f14f8a7e80ba
abubjazov <abubjazov@mail.ru>
2022-02-27 23:47:11 +0300

    Navbar: Navbar.test refactor

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -5,17 +5,27 @@ import { screen } from '@testing-library/react'
 import { Navbar } from './Navbar'
 
 describe('Navbar Test', () => {
-	test('should render with correct values', async () => {
+	test('about link test', async () => {
 			renderWithRouter(<Navbar />)
-			const mainLink = screen.getByTestId('main-link')
 			const aboutLink = screen.getByTestId('about-link')
-			const usersLink = screen.getByTestId('users-link')
 
 			userEvent.click(aboutLink)
 			expect(screen.getByTestId('about-page')).toBeInTheDocument()
-			userEvent.click(mainLink)
-			expect(screen.getByTestId('main-page')).toBeInTheDocument()
-			userEvent.click(usersLink)
-			expect(screen.getByTestId('users-list')).toBeInTheDocument()
+	})
+
+	test('main link test', async () => {
+		renderWithRouter(<Navbar />)
+		const mainLink = screen.getByTestId('main-link')
+
+		userEvent.click(mainLink)
+		expect(screen.getByTestId('main-page')).toBeInTheDocument()
+	})
+
+	test('users link test', async () => {
+		renderWithRouter(<Navbar />)
+		const usersLink = screen.getByTestId('users-link')
+
+		userEvent.click(usersLink)
+		expect(screen.getByTestId('users-list')).toBeInTheDocument()
 	})
 })
