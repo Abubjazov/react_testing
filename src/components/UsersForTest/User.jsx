930a1981d002e192c1c6834c73e3fd16d6ba0e7b
abubjazov <abubjazov@mail.ru>
2022-03-03 23:14:20 +0300

    components: +User

---
+++ src/components/UsersForTest/User.jsx
@@ -0,0 +1,8 @@
+export const User = ({user, onDelete}) => {
+    return (
+        <div>
+            {user.name}
+            <button id="user-delete" onClick={() => onDelete(user.id)}>delete</button>
+        </div>
+    )
+}
