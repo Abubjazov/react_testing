041b68aa741af9bc14dbed460ec3ff4eaeac6c44
abubjazov <abubjazov@mail.ru>
2022-03-03 23:41:47 +0300

    proj/tests/e2e: users.e2e+

--- tests/pages/users.page.js
+++ tests/pages/users.page.js
@@ -16,10 +16,30 @@ class UsersPage extends Page {
     async loadData() {
         try {
             await this.open()
-            await this.loadingTitle.waitForDisplayed({timeout: 3000})
             await this.loadingTitle.waitForDisplayed({timeout: 2000})
-        } catch (error) {
-            throw new Error('Неудалось загрузить пользователей')
+            await this.usersList.waitForDisplayed({timeout: 2000})
+        } catch (e) {
+            throw new Error('Не удалось загрузить пользователей')
+        }
+    }
+    async deleteUser() {
+        try {
+            const usersCount = await this.usersItems.length
+
+            if(!usersCount) {
+                throw new Error('Пользователи не найдены')
+            }
+
+            await this.usersItems[0].$('#user-delete').click()
+
+            const usersCountAfterDelete = await this.usersItems.length
+
+            if(usersCount - usersCountAfterDelete !== 1) {
+                throw new Error('Удаление не произошло или удалился более чем 1 пользователь')
+            }
+
+        } catch (e) {
+            throw new Error('Не удалось удалить пользователя. ' + e.message)
         }
     }
 
